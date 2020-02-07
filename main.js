742cdb70eeb5a690508f4de9a0383096d678f2aa
Md.Mahabub Al Islam <32910324+pasha13d@users.noreply.github.com>
2020-02-07 20:56:30 +0600

    all files of issue tracker

---
+++ main.js
@@ -0,0 +1,79 @@
+document.getElementById('issueInputForm').addEventListener('submit', saveIssue);
+
+function saveIssue(e) {
+    var issueDesc = document.getElementById('issueDescInput').value;
+    var issueSeverity = document.getElementById('issueSeverityInput').value;
+    var issueAssignedTo = document.getElementById('issueAssignedToInput').value;
+    var issueId = chance.guid();
+    var issueStatus = 'Open';
+
+    var issue = {
+        id: issueId,
+        desc: issueDesc,
+        severity: issueSeverity,
+        assignedTo: issueAssignedTo,
+        status: issueStatus
+    }
+
+    if (localStorage.getItem('issues') == null) {
+        var issues = [];
+        issues.push(issue);
+        localStorage.setItem('issues', JSON.stringify(issues));
+    } else {
+        var issues = JSON.parse(localStorage.getItem('issues'));
+        issues.push(issue);
+        localStorage.setItem('issues', JSON.stringify(issues));
+    }
+    document.getElementById('issueInputForm').reset();
+
+    fetchIssues();
+
+    e.preventDefault();
+}
+
+function setStatusClosed(id) {
+    var issues = JSON.parse(localStorage.getItem('issues'));
+    for (var i = 0; i < issues.length; i++) {
+        if (issues[i].id == id) {
+            issues[i].status = 'Closed';
+        }
+    }
+    localStorage.setItem('issues', JSON.stringify(issues));
+    fetchIssues();
+}
+
+function deleteIssue(id) {
+    var issues = JSON.parse(localStorage.getItem('issues'));
+    for (var i = 0; i < issues.length; i++) {
+        if (issues[i].id == id) {
+            issues.splice(i, 1);
+        }
+    }
+    localStorage.setItem('issues', JSON.stringify(issues));
+    fetchIssues();
+}
+
+function fetchIssues() {
+    var issues = JSON.parse(localStorage.getItem('issues') || "[]");
+    var issuesList = document.getElementById('issueList');
+
+    issuesList.innerHTML = '';
+
+    for (var i = 0; i < issues.length; i++) {
+        var id = issues[i].id;
+        var desc = issues[i].description;
+        var severity = issues[i].severity;
+        var assignedTo = issues[i].assignedTo;
+        var status = issues[i].status;
+
+        issuesList.innerHTML += '<div class="card card-body bg-light">'
+                                + '<h6> Issue ID: ' + id + '</h6>'
+                                + '<p><span class="badge badge-info">' + status + '</span></p>'
+                                + '<h3>' + desc + '</h3>'
+                                + '<p><span class="fa fa-clock-o"></span>&nbsp' + severity + '</p>'
+                                + '<p><span class="fa fa-user"></span> &nbsp' + assignedTo + '</p>'
+                                + '<div><a href="#" onclick="setStatusClosed(\''+id+'\')" class="btn btn-warning col-md-1">Close</a> &nbsp' 
+                                + '<a href="#" onclick="deleteIssue(\''+id+'\')" class="btn btn-danger col-md-1">Delete</a></div>'
+                                + '</div> <br/>';
+    }
+}
